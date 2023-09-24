2ae8a8a17aaad589eff648ad865b4c5b2f3f4357
Abhishek Bisht <abhishek.bisht54@yahoo.com>
2023-09-24 11:38:36 +0530

    added routes

--- index.js
+++ index.js
@@ -1,76 +1,25 @@
-const dotenv = require('dotenv');
-dotenv.config();
-const { application } = require("express");
+require("dotenv").config();
 const express = require("express");
-const bodyParser = require('body-parser');
-
 const PORT = process.env.PORT;
-console.log(PORT);
+
+const moviesRouter = require("./routes/movies");
+
+console.log({ PORT });
 const app = express();
 
-const movies = [
-	{
-		name: "Dhoom",
-		year: "2008",
-		director: "Yash Raj",
-		genre: "Action",
-		rate: "9.2"
-	},
-	{
-		name: "Dhoom 2",
-		year: "2014",
-		director: "Yash Raj",
-		genre: "Action",
-		rate: "9.2"
-	}
-]
-
-
 app.listen(PORT, () => {
-	console.log(`Server is running on ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
 });
 
 const logger = (req, res, next) => {
-	console.log(`Request received at ${new Date()} on Path ${req.url}`);
-	next();
-}
+  console.log(`Request received at ${new Date()} on path ${req.url}`);
+  next();
+};
 
 app.use(logger);
-app.use(bodyParser.json());
-
-app.get("/", (req, res) => {
-	res.send("Hello World!");
-});
-
-app.get("/api/movies", (req, res) => {
-	res.send(movies);
-});
-
-app.get("/api/movies/:id", (req, res) => {
-	const id = req.params.id;
-	res.send(movies[parseInt(id)-1]);
-});
-
-app.post("/api/movies", (req,res) => {
-	const movie = req.body
-	movie.id = movies.length + 1;
-	movies.push(movie);
-	console.log(movies);
-	res.send({success: true});
-});
-
-app.put("/api/movies/:id", (req,res) => {
-	const id  = req.params.id;
-	const body = req.body;
-	body.id = id;
-	movies[id-1] = body;
-	console.log(movies);
-	res.send({success: true});
-});
-
-
-app.delete("/api/movies/:id", (req, res) => {
-  const id = req.params.id;
-  movies.splice(id - 1, 1);
-  res.send({ success: true });
-});
+app.use(express.json());
+app.use(moviesRouter);
